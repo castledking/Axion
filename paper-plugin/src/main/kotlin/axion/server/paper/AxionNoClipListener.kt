97ba83975c270f0b3a94183399cc4f297a7f634d
castledking <thecastledking@gmail.com>
2026-03-25 10:43:26 -0700

    fix: keep Paper noclip movement server-synced while phasing

--- paper-plugin/src/main/kotlin/axion/server/paper/AxionNoClipListener.kt
+++ paper-plugin/src/main/kotlin/axion/server/paper/AxionNoClipListener.kt
@@ -1,5 +1,7 @@
 package axion.server.paper
 
+import io.papermc.paper.event.player.PlayerFailMoveEvent
+import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
@@ -10,5 +12,31 @@ class AxionNoClipListener(
     @EventHandler
     fun onPlayerQuit(event: PlayerQuitEvent) {
         noClipService.clear(event.player)
+    }
+
+    @EventHandler
+    fun onPlayerFailMove(event: PlayerFailMoveEvent) {
+        val player = event.player
+        if (!noClipService.shouldEnableNoPhysics(player)) {
+            return
+        }
+
+        if (event.failReason == PlayerFailMoveEvent.FailReason.MOVED_INTO_UNLOADED_CHUNK) {
+            return
+        }
+
+        val to = event.to
+        if (!player.world.isChunkLoaded(to.blockX shr 4, to.blockZ shr 4)) {
+            return
+        }
+
+        if (event.failReason == PlayerFailMoveEvent.FailReason.MOVED_TOO_QUICKLY) {
+            event.isAllowed = true
+            return
+        }
+
+        if (player.gameMode == GameMode.CREATIVE && player.isFlying) {
+            event.isAllowed = true
+        }
     }
 }
